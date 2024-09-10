20e9dcdc685d42f653517daf8148cf234ac53437
abigail <abigail@abigaildeMacBook-Air.local>
2024-09-09 23:51:24 -0400

    check

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        int number = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0) {
+                number++;
+            } else {
+                if (i % 5 == 0) {
+                    number++;
+                }
+            }
+
+        }
+        System.out.println(number);
+    }
 }
